f243241d8f432818fbe1868a73d9e876ae423522
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-10-23 15:18:48 -0400

    project created with beforeEach in test

--- test/capICO.js
+++ test/capICO.js
@@ -7,15 +7,19 @@
   }
 
   describe('capICO', () => {
+    let capico
 
+    beforeEach(async () => {
+        const capICO = await ethers.getContractFactory('capICO') // pull in contract from hardhat
+        capico = await capICO.deploy() 
+    })
 
     describe('Deployment', () => {
       it('has correct name', async () => {
-        const capICO = await ethers.getContractFactory('capICO') // pull in contract from hardhat
-        let capico = await capICO.deploy() //we deploy it an assign it to a lower case variable
+     
         expect(await capico.name()).to.equal('capICO') 
 
-        console.log('retreiving address...')
+        //console.log('verifying name...')
         //calling the name function (we have this funct. automatically once declared in contr.)
       })
     })
