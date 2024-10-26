724c029264c9b1a9a27a330f26022bd546777a6c
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-10-26 10:51:45 -0400

    Initial Nav formatting. Nav still in Appjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-
-const message = "Hello, world!"
+import { Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../logo.png';
 
 function App() {
   return(
-  <div>
-  <div>{message}</div>
-  <div>{1 + 1}</div>
-  </div>
+    <Container>
+    <Navbar>
+      <img alt="logo" 
+      src={logo} 
+      width="40" 
+      height="40" 
+      className= "d-inline-block align-top mx-3"
+      />
+      <Navbar.Brand href='#'>CKOIN ICO Crowdsale</Navbar.Brand>
+      </Navbar>
+    </Container>
   )
-
 }
-
-
 
 export default App;
