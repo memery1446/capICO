6bb76b181fd3decce4df9b23f4cb8674c68e7825
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-10-26 10:12:59 -0400

    deployment to node. Initiation of deployment script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,16 +7,29 @@
 const hre = require("hardhat");
 
 async function main() {
-  const NAME = 'Dapp University'
-  const SYMBOL = 'DAPP'
+  const NAME = 'CKOIN Token'
+  const SYMBOL = 'CKOIN'
   const MAX_SUPPLY = '1000000'
+  const PRICE = ethers.utils.parseUnits('0.025', 'ether')
 
-  // Deploy Token
   const Token = await hre.ethers.getContractFactory('Token')
-  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
-
+  let token = await Token.deploy('CKOIN TOKEN', 'CKOIN', '1000000')
   await token.deployed()
+
   console.log(`Token deployed to: ${token.address}\n`)
+
+  const capICO = await hre.ethers.getContractFactory('capICO')
+  const capico = await capICO.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await capico.deployed()
+
+  console.log(`capICO deployed to: ${capico.address}\n`)
+
+  // Tokens to ICO
+
+  const transaction = await token.transfer(capico.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await transaction.wait()
+
+  console.log('Tokens transferrred to ICO')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
