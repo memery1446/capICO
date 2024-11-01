eaa1d979a6e95fed03b126cf1c33fb2a9e2cfb3f
Mark Emery <markemery@Marks-MacBook-Pro.local>
2024-11-01 18:10:35 -0400

    much work then return to base level

--- contracts/capICO.sol
+++ contracts/capICO.sol
@@ -4,112 +4,62 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "./Token.sol";
 
-
 contract capICO {
-    address public owner;
+    address owner;
     Token public token;
     uint256 public price;
     uint256 public maxTokens;
     uint256 public tokensSold;
-    uint256 public tokensClaimed;
-    uint256 public goal; 
 
-    mapping(address => uint256) public tokensOwned; 
+    event Buy(uint256 amount, address buyer);
+    event Finalize(uint256 tokensSold, uint256 ethRaised);
 
-event Buy(uint256 amount, address buyer); 
-event Claimed(uint256 amount, address beneficiary);
-event Finalize(uint256 tokensSold, uint256 ethRaised);
-
-constructor(
-    Token _token, 
-    uint256 _price, 
-    uint256 _maxTokens, 
-    uint256 _goal
-
+    constructor(
+        Token _token,
+        uint256 _price,
+        uint256 _maxTokens
     ) {
+        owner = msg.sender;
+        token = _token;
+        price = _price;
+        maxTokens = _maxTokens;
+    }
 
-    token = _token;
-    price = _price;
-    maxTokens = _maxTokens;
-    goal = _goal; 
-    owner = msg.sender;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+
+    // Buy tokens directly by sending Ether
+    // --> https://docs.soliditylang.org/en/v0.8.15/contracts.html#receive-ether-function
+
+    receive() external payable {
+        uint256 amount = msg.value / price;
+        buyTokens(amount * 1e18);
+    }
+
+    function buyTokens(uint256 _amount) public payable {
+        require(msg.value == (_amount / 1e18) * price);
+        require(token.balanceOf(address(this)) >= _amount);
+        require(token.transfer(msg.sender, _amount));
+
+        tokensSold += _amount;
+
+        emit Buy(_amount, msg.sender);
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    // Finalize Sale
+    function finalize() public onlyOwner {
+        require(token.transfer(owner, token.balanceOf(address(this))));
+
+        uint256 value = address(this).balance;
+        (bool sent, ) = owner.call{value: value}("");
+        require(sent);
+
+        emit Finalize(tokensSold, value);
+    }
 }
-
-modifier onlyOwner() {
-    require(msg.sender == owner, 'only the owner can call this function');
-
-    _; 
-}
-
-
-receive() external payable {
-    uint256 amount = msg.value / price;
-    buyTokens(amount * 1e18); 
-}
-
-function buyTokens(uint256 _amount) public payable {
-        // Converting the price
-    require(msg.value == (_amount / 1e18) * price);
-
-        // Require that the contract has enough tokens
-    require(token.balanceOf(address(this)) >= _amount);
-
-        // Update users address with tokensOwned
-    tokensOwned[msg.sender] = _amount;
-
-        // Keep track of tokensSold
-    tokensSold += _amount;
-
-    emit Buy(_amount, msg.sender);
-}
-
-function setPrice(uint256 _price) public onlyOwner {
-    price = _price;
-}
-
-function claimTokens(address beneficiary) public payable returns (uint256 _amount) {
-        // Assign value to beneficiary
-    require(beneficiary == msg.sender);
-        // Access the mapping
-    tokensOwned[msg.sender] = _amount; 
-        // Transfer the tokens to the beneficiary
-    require(token.transfer(msg.sender, _amount));
-        // Keep track of tokensSold
-    tokensSold += _amount;
-        // Keep track of tokensClaimed
-    tokensClaimed += _amount;
-
-    emit Claimed(_amount, msg.sender);
-
-
-}
-
-function claimRefund(address refundedTo) public payable returns (uint256 _amount) {
-
-    require(refundedTo == msg.sender);
-    tokensOwned[msg.sender] = _amount;
-            // Require that the contract has enough tokens
-    require(token.balanceOf(address(this)) >= _amount);
-    require(msg.value == (_amount / price) * 1e18);
-
-
-}
-
-function finalize () public onlyOwner {
-
-        // Caller must be the owner (redundant w/ mod.)
-    require(msg.sender == owner);
-        // Determine tokens to be sent to owner
-
-    require(token.transfer(owner, token.balanceOf(address(this))));
-
-    uint256 value = address(this).balance;
-    (bool sent, ) = owner.call{value: value}("");
-    require(sent);
-
-    emit Finalize(tokensSold, value);
-
-}
-
-}
-
--- test/capICO.js
+++ test/capICO.js
@@ -1,39 +1,35 @@
-  const { expect } = require('chai'); 
-  const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-  const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether');
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
+const ether = tokens
 
-  }
-  const ether = tokens
+describe('capICO', () => {
+  let token, capico
+  let deployer, user1
 
-  describe('capICO', () => {
-    let capico, token
-    let deployer, user1
+  beforeEach(async () => {
+    const capICO = await ethers.getContractFactory('capICO')
+    const Token = await ethers.getContractFactory('Token')
 
-    beforeEach(async () => {
-        const capICO = await ethers.getContractFactory('capICO') // pull in contract from hardhat
-        const Token = await ethers.getContractFactory('Token')
-        
-        token = await Token.deploy('CKOIN Token', 'CKOIN', '1000000')
-      
+    token = await Token.deploy('CKOIN Token', 'CKOIN', '1000000')
 
-        accounts = await ethers.getSigners()
-        deployer = accounts[0]
-        user1 = accounts[1]
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    user1 = accounts[1]
 
-
-
-    capico = await capICO.deploy(token.address, ether(1), '1000000', '500000')
+    capico = await capICO.deploy(token.address, ether(1), '1000000')
 
     let transaction = await token.connect(deployer).transfer(capico.address, tokens(1000000))
     await transaction.wait()
-    })
+  })
 
   describe('Deployment', () => {
 
-    it('sends tokens to the capICO contract', async () => {
+    it('sends tokens to the Crowdsale contract', async () => {
       expect(await token.balanceOf(capico.address)).to.equal(tokens(1000000))
     })
 
@@ -45,7 +41,7 @@
       expect(await capico.token()).to.equal(token.address)
     })
 
-    })
+  })
 
   describe('Buying Tokens', () => {
     let transaction, result
@@ -58,137 +54,119 @@
         result = await transaction.wait()
       })
 
-      it('updates users address mapping with token amount', async () => {
-        expect(await capico.tokensOwned(user1.address)).to.equal(amount)
+      it('transfers tokens', async () => {
+        expect(await token.balanceOf(capico.address)).to.equal(tokens(999990))
+        expect(await token.balanceOf(user1.address)).to.equal(amount)
       })
 
-      it('updates ICO tokens sold', async () => {
-         expect(await capico.tokensSold()).to.equal(amount)
-      })
-
-      it('updates the contracts ether balance', async () => {
-        expect(await ethers.provider.getBalance(capico.address)).to.equal(amount)
-      })
-
-      it('updates the number of tokens owned', async () => {
-        expect(await capico.tokensOwned(user1.address)).to.equal(amount)
+      it('updates tokensSold', async () => {
+        expect(await capico.tokensSold()).to.equal(amount)
       })
 
       it('emits a buy event', async () => {
-        //console.log(result)
-        await expect(transaction).to.emit(capico, 'Buy').withArgs(amount, user1.address)
+        // --> https://hardhat.org/hardhat-chai-matchers/docs/reference#.emit
+        await expect(transaction).to.emit(capico, "Buy")
+          .withArgs(amount, user1.address)
       })
+
     })
 
-  describe('Failure', () => {
-      it('rejects insufficient ETH', async () => {
+    describe('Failure', () => {
+
+      it('rejects insufficent ETH', async () => {
         await expect(capico.connect(user1).buyTokens(tokens(10), { value: 0 })).to.be.reverted
       })
 
-      it('rejects purchases over the maxSupply', async () => {
-        await expect(capico.connect(user1).buyTokens(tokens(1200000), {value: 1200000 })).to.be.reverted
-      })
     })
+
   })
 
   describe('Sending ETH', () => {
     let transaction, result
-    let amount = ether(10) 
+    let amount = ether(10)
 
     describe('Success', () => {
+
       beforeEach(async () => {
         transaction = await user1.sendTransaction({ to: capico.address, value: amount })
         result = await transaction.wait()
       })
 
-      it('updates contract eth balance', async () => {
+      it('updates contracts ether balance', async () => {
         expect(await ethers.provider.getBalance(capico.address)).to.equal(amount)
       })
 
-      it('updates the user tokens owned and sold', async () => {
-        expect(await capico.tokensOwned(user1.address)).to.equal(amount)
-       // expect(await capico.tokensSold(deployer.address)).to.equal(amount)
+      it('updates user token balance', async () => {
+        expect(await token.balanceOf(user1.address)).to.equal(amount)
       })
+
     })
   })
 
   describe('Updating Price', () => {
-  let transaction, result
-  let price = ether(2)
-
-  describe('Success', () => {
-
-    beforeEach(async () => {
-      transaction = await capico.connect(deployer).setPrice(ether(2))
-      result = await transaction.wait()
-    })
-
-    it('updates the price', async () => {
-      expect(await capico.price()).to.equal(ether(2))
-    })
-  })
-
-  describe('Failure', () => {
-    it('prevents a non-owner from updating price', async () => {
-      await expect(capico.connect(user1).setPrice(price)).to.be.reverted
-    })
-  })
-
-  describe('Claiming Tokens', () => {
     let transaction, result
+    let price = ether(2)
 
-    describe('Success', async () => {
+    describe('Success', () => {
+
       beforeEach(async () => {
-       
+        transaction = await capico.connect(deployer).setPrice(ether(2))
+        result = await transaction.wait()
+      })
 
-    })
+      it('updates the price', async () => {
+        expect(await capico.price()).to.equal(ether(2))
+      })
 
-    })
-    
-  })
-})
-
-  describe('Finalizing the sale', () => {
-    let transaction, result
-    let amount = tokens(10)  
-    let value = ether(10)
-
-
-  describe('Success', async () => {
-    beforeEach(async () => {
-      transaction = await capico.connect(user1).buyTokens(amount, { value: value })
-      result = await transaction.wait()
-
-      transaction = await capico.connect(deployer).finalize()
-      result = await transaction.wait()
-    })
-
-    it('transfers remaining tokens to owner', async () => {
-          expect(await token.balanceOf(capico.address)).to.equal(0)
-          //expect(await token.balanceOf(deployer.address)).to.equal(tokens(999990))
-        })
-
-    it('transfers ETH balance to owner', async () => {
-      expect(await ethers.provider.getBalance(capico.address)).to.equal(0)
-    })
-
-    it('emits a finalize event', async () => {
-    await expect(transaction).to.emit(capico, "Finalize")
-      .withArgs(amount, value)
-  })
     })
 
     describe('Failure', () => {
-      it('prevents a non-owner from finalizing', async () => {
-        // comment in and comment out two lines below for failure case
-        // await capico.connect(user1).finalize()
-        await expect(capico.connect(user1).finalize()).to.be.reverted
-    })
-})
 
+      it('prevents non-owner from updating price', async () => {
+        await expect(capico.connect(user1).setPrice(price)).to.be.reverted
+      })
+
+    })
   })
 
+  describe('Finalzing Sale', () => {
+    let transaction, result
+    let amount = tokens(10)
+    let value = ether(10)
+
+    describe('Success', async () => {
+
+      beforeEach(async () => {
+        transaction = await capico.connect(user1).buyTokens(amount, { value: value })
+        result = await transaction.wait()
+
+        transaction = await capico.connect(deployer).finalize()
+        result = await transaction.wait()
+      })
+
+      it('transfers remaining tokens to owner', async () => {
+        expect(await token.balanceOf(capico.address)).to.equal(0)
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999990))
+      })
+
+      it('transfers ETH balance to owner', async () => {
+        expect(await ethers.provider.getBalance(capico.address)).to.equal(0)
+      })
+
+      it('emits Finalize event', async () => {
+        // --> https://hardhat.org/hardhat-chai-matchers/docs/reference#.emit
+        await expect(transaction).to.emit(capico, "Finalize")
+          .withArgs(amount, value)
+      })
+
+    })
+
+    describe('Failure', () => {
+
+      it('prevents non-owner from finalizing', async () => {
+        await expect(capico.connect(user1).finalize()).to.be.reverted
+      })
+
+    })
+  })
 })
-
-
-
